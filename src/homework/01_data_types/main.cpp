c4adcba38ab19320dd36b74e944e3e5f4802616e
8962604 <omar.farooq@g.austincc.edu>
2024-09-11 22:26:56 +0000

    homework 1

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,28 @@
 ﻿//write include statements
+# include <iostream>
+# include "data_types.h" // tells C++ where to find multiply_numbers function(like the import function from python)
 
 
 //write namespace using statement for cout
-
+using std::cout, using std::cin
 
 int main()
-{
-	
 
-	return 0;
+{
+	int num;
+	cout<<"Enter a number;";
+	cin>>num;
+
+	int result = multiply_numbers(num);
+
+	cout<<"Your product is: "<<result<<"\n";
+
+	int num1 = 4;
+
+	result = multiply_numbers(num1);
+
+	cout<<"Your product is: "<<result<<"\n";
+
+    return 0;
 }
+
