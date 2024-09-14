fe94f7335c5899dd4d728ff2f4e386381d7c4ae1
8962604 <omar.farooq@g.austincc.edu>
2024-09-14 23:29:41 +0000

    homework 2 changes

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -4,7 +4,7 @@
 
 
 //write namespace using statement for cout
-using std::cout; using std::cin;
+using namespace std;
 
 int main()
 
@@ -21,9 +21,9 @@ int main()
 	cin>>tip_rate;
 	tip_amount = get_tip_amount(meal_amount, tip_rate/100);
 	total = tip_amount + tax_amount + meal_amount;
-	cout<< "Meal Amount: "<<meal_amount;
-	cout<< "Sales Tax: "<<tax_amount;
-	cout<< "Tip Amount: "<<tip_amount;
-	cout<< "Total: "<<total;
+	cout<< "Meal Amount: "<<meal_amount << endl;
+	cout<< "Sales Tax: "<<tax_amount << endl;
+	cout<< "Tip Amount: "<<tip_amount << endl;
+	cout<< "Total: "<<total << endl;
   return 0;
 }
