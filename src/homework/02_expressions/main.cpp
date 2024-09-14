df145521112c17a0deae800c9ee5e899af59c841
8962604 <omar.farooq@g.austincc.edu>
2024-09-14 22:20:38 +0000

    homework 2 corrections

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -19,16 +19,11 @@ int main()
 	tax_amount = get_sales_tax_amount(meal_amount);
 	cout<<"Enter a tip rate: ";
 	cin>>tip_rate;
-	cout<<meal_amount;
-	cout<<tip_rate;
-	cout<<tip_amount;
-	cout<<tax_amount;
-	cout<<total;
 	tip_amount = get_tip_amount(meal_amount, tip_rate/100);
 	total = tip_amount + tax_amount + meal_amount;
-	cout<< "Meal Amount:  ";
-	cout<< "Sales Tax: ";
-	cout<< "Tip Amount: ";
-	cout<< "Total Amount: ";
+	cout<< "Meal Amount: "<<meal_amount;
+	cout<< "Sales Tax: "<<tax_amount;
+	cout<< "Tip Amount: "<<tip_amount;
+	cout<< "Total: "<<total;
   return 0;
 }
