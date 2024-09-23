d700bd69be9c443675b0af53abf376b2601dc251
Omar farooq <omar.farooq@g.austincc.edu>
2024-09-23 18:53:52 -0500

    for loop changes

--- src/examples/04_repetition/02_for/for.cpp
+++ src/examples/04_repetition/02_for/for.cpp
@@ -8,7 +8,19 @@ void display_hello_for(int num)
  {
     cout<<"Hello\n";
  }
- 
+
+}
+int sum_of_squares_for(int num)
+{
+ auto total = 0;
+
+
+ for(auto i = 0; i <= num; i++)
+ {
+   total += i * i;
+
+ }
+ return total;
 }
 
 //Write code for function display_numbers that accepts a number and 
--- test/examples/04_repetition/04_repetition_tests.cpp
+++ test/examples/04_repetition/04_repetition_tests.cpp
@@ -7,6 +7,12 @@ TEST_CASE("Verify Test Configuration", "verification") {
 TEST_CASE("Verify sum of squares")
 {
    REQUIRE(14 == sum_of_squares(3));
-   REQUIRE(30 == sum_of_squares(4));
-   REQUIRE(35 == sum_of_squares(5));
+   REQUIRE(50 == sum_of_squares(4));
+   REQUIRE(55 == sum_of_squares(5));
+}
+TEST_CASE("Verify sum of squares for")
+{
+   REQUIRE(14 == sum_of_squares_for(3));
+   REQUIRE(50 == sum_of_squares_for(4));
+   REQUIRE(55 == sum_of_squares_for(5));
 }
