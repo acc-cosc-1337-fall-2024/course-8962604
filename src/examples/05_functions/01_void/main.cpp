6f14a4f1505189804b434b2bf196882861fd43e7
8962604 <omar.farooq@g.austincc.edu>
2024-09-30 23:31:22 +0000

    main.cpp changes

--- src/examples/05_functions/01_void/main.cpp
+++ src/examples/05_functions/01_void/main.cpp
@@ -1,4 +1,9 @@
+#include "void.h"
+
+using std::string;
 int main()
 {
+    string lang = "Python";
+    display_string_characters(string);
     return 0;
 }
---
+++ src/examples/05_functions/01_void/void.cpp
@@ -0,0 +1,16 @@
+#include "void.h"
+
+using std::string;
+
+void void display_string_characters(const string str)
+{
+    for (auto i=0; i < str.size(); i++)
+    {
+
+        cout<<str.size[i]<<"\n";
+
+    }
+
+
+
+}
