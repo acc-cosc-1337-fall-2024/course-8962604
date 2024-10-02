6a68d86c87aed8905ad90b14e85d74273f53b805
8962604 <omar.farooq@g.austincc.edu>
2024-10-02 23:17:16 +0000

    changes to int num

--- src/examples/05_functions/02_references/main.cpp
+++ src/examples/05_functions/02_references/main.cpp
@@ -5,6 +5,13 @@ using std::cout;
 int main()
 {
     int num = 5;
+    int num1 = 10;
+
     cout<<num<<"\n";
+    cout<<"num is stored at memory address: "<<&num<<"\n";
+
+    cout<<num1<<"\n";
+    cout<<"num1 is stored at memory address: "<<&num1<<"\n";
+
     return 0;
 }
