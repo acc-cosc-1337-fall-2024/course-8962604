0c96a4d9a4a3b4961221fcb4262e13c23699a753
8962604 <omar.farooq@g.austincc.edu>
2024-10-02 23:14:02 +0000

    changes to variable

--- src/examples/05_functions/01_void/void.cpp
+++ src/examples/05_functions/01_void/void.cpp
@@ -11,8 +11,6 @@ void display_string_characters(const std::string str)
 
     }
 
-
-
 }
 void replace_character_in_string(std::string str, char target, char replace_value)
 {
--- src/examples/05_functions/02_references/main.cpp
+++ src/examples/05_functions/02_references/main.cpp
@@ -1,4 +1,10 @@
+#include<iostream>
+
+using std::cout;
+
 int main()
 {
+    int num = 5;
+    cout<<num<<"\n";
     return 0;
 }
