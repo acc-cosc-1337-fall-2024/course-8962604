3f9fad692f39b11f0d4eaad853a5b34aa6d55f36
8962604 <omar.farooq@g.austincc.edu>
2024-10-12 13:58:00 +0000

    max int calc

--- src/examples/02_expressions/03_overflow/main.cpp
+++ src/examples/02_expressions/03_overflow/main.cpp
@@ -6,5 +6,12 @@ using std::cout; using std::cin;
 
 int main()
 {
+    int max_int = INT_MAX;
+    // Add 1 to max_int
+    cout << "Value of max_int" << max_int;
+
+    max_int += 1;
+    cout<<"Value of max_int" << max_int;
     
+    return 0;
 }
