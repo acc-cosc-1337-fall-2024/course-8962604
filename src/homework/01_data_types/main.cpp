e33396a5d4a2210a1a0dd97132e2d4e52a593ef3
8962604 <omar.farooq@g.austincc.edu>
2024-10-12 12:44:53 +0000

    add more code later

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -8,19 +8,5 @@ using namespace std;
 using std::cout; using std::cin
 int main()
 {
-	int num;
-	cout<<"Enter a number;";
-	cin>>num,
 
-	int result = multiply_numbers(num);
-
-	cout<<"Your product is: "<<result<<"\n";
-
-	int num1 = 4;
-
-	result = multiply_numbers(num1);
-
-	cout<<"Your product is: "<<result<<"\n";
-
-    return 0;
 }
