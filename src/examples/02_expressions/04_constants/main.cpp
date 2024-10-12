c4503381f546e728b1cb562a7dc529bee979358b
8962604 <omar.farooq@g.austincc.edu>
2024-10-12 21:55:42 +0000

    changes to main.cpp

--- src/examples/02_expressions/04_constants/main.cpp
+++ src/examples/02_expressions/04_constants/main.cpp
@@ -4,11 +4,11 @@ using std::cout; using std::cin;
 int main()
 {
     int value;
-    cout<<,"Enter a number";
+    cout<<"Type a number: ";
     cin>>value;
 
     double result = calculate_interest_rate(value);
     cout<<result<<"\n";
-    
+
     return 0;
 }
