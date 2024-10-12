dadab5b486aca541dbc657ad7c5c27d5abc85ff4
8962604 <omar.farooq@g.austincc.edu>
2024-10-12 19:14:29 +0000

    adding books sold monthly to int.h

--- src/examples/01_data_types/01_int/int.h
+++ src/examples/01_data_types/01_int/int.h
@@ -1,6 +1,7 @@
 //Create prototype for void function echo_variable with one int parameter
 int echo_variable(int num);
 int add_numbers(int num1, int num2);
+double avg_books_sold_monthly(int books, int months);
 int divide_numbers(double num1, double num2);
 double calculate_interest(double rate, int amount);
 int return_ascii_value(char ch);
