c536fafe71c3cd98c0c888e4f8882578b405e437
8962604 <omar.farooq@g.austincc.edu>
2024-10-12 12:20:46 +0000

    if changes

--- src/examples/03_conditionals/01_if/if.h
+++ src/examples/03_conditionals/01_if/if.h
@@ -1,5 +1,2 @@
 //write function prototype for function named overtime that accepts a double
 // and returns a bool type
-bool_is_vowel(char letter);
-bool_is_consonant(char letter);
-bool_is_even(int num);
