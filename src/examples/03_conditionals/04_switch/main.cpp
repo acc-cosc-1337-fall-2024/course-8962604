c1cbbb53c16d2433a9744003594c0c68b8daf92c
8962604 <omar.farooq@g.austincc.edu>
2024-10-19 20:24:45 +0000

    enum_types

--- src/examples/03_conditionals/04_switch/main.cpp
+++ src/examples/03_conditionals/04_switch/main.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 #include "switch.h"
 
-using std::cout; using std::cin; using std::string;
+using std::cout; using std::cin; 
+
+using std::string;
 
 int main() 
 {
+    int option;
+    cout<<"Enter Option:";
+    cin>>option;
+    string option_result = menu_option(option);
+    cout<<option_result<<"\n";
+    return 0;
 
 }
