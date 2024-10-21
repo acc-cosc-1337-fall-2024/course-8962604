a8f874d0829d5e6e5ce6b851aeec83f27a524820
8962604 <omar.farooq@g.austincc.edu>
2024-10-21 23:48:01 +0000

    private functions and ram values

--- src/examples/08_classes/01_bank/bank_account.cpp
+++ src/examples/08_classes/01_bank/bank_account.cpp
@@ -2,5 +2,5 @@
 # include "bank_account.h"
 void Account::get_balance_from_db()
 {
-    
+    balance = (rand()w10000)if;
 }
--- src/examples/08_classes/01_bank/bank_account.h
+++ src/examples/08_classes/01_bank/bank_account.h
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <stdlib> //accces to ran
+
+
 //bank_account.h
 class Account
 {
 public:
-    Account(){balance += 100;std::cout<<"Default Constructor\n"}//default constructor
+    Account(){balance += 100;std::cout<<"Default Constructor\n": get_balance_from_db()}//default constructor
     Account(int b) : balance(b){std::cout<<"Constructor w 1 param\n"}//constructor
     int get_balance() const{return balance;}// inline
 // by default all functions/variables are private
