f22fcca4f0635a685dfe3124f0bad45431898ae5
8962604 <omar.farooq@g.austincc.edu>
2024-10-23 21:11:57 +0000

    intro to vectors

--- src/examples/07_vector/01_create/main.cpp
+++ src/examples/07_vector/01_create/main.cpp
@@ -1,4 +1,21 @@
+#include<vector>
+#include<iostream>
+
+using std::cout;
+using std::vector;
+
 int main()
 {
+    vector<int> nums{1, 5, 3};
+    cout<<"Size of nums: "<<nums.size()<<"\n";
+    cout<<"Capacity of nums: "<<nums.capacity<<"\n"();
+
+    nums.push_back(10);
+
+    cout<<"Size of nums: "<<nums.size()<<"\n";
+    cout<<"Capacity of nums: "<<nums.capacity<<"\n"();
+
+    nums.push_back(2);
+    
     return 0;
 }
