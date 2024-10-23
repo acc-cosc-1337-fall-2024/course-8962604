26fcace26661379c13a7a1c2aba7286af2840c3f
8962604 <omar.farooq@g.austincc.edu>
2024-10-23 12:14:15 +0000

    introduction to strings

--- src/examples/06_strings/01_create/main.cpp
+++ src/examples/06_strings/01_create/main.cpp
@@ -1,6 +1,22 @@
 # include<iostream>
 # include<string>
+
+using std::cout;
+using std::string;
+
 int main()
 {
-  
+    string lang = "C++";
+    auto num = 5;
+
+    cout<<lang<<"\n";
+    cout<<"Address of lang"<<&lang<<"\n";
+    cout<<"Addres of num"<<&num<<"\n";
+
+    cout<<lang[0]<<"\n";
+    cout<<"Address of character 0 index at C: "<<static_cast<void*>(&lang[0])<<"\n";
+    cout<<"Address of character 0 index at +: "<<static_cast<void*>(&lang[1])<<"\n";
+    cout<<"Address of character 0 index at +: "<<static_cast<void*>(&lang[2])<<"\n";
+
+  return 0;
 }
