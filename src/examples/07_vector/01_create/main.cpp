a8687aa7834903f421b450c97fee00c8d5ad07c4
8962604 <omar.farooq@g.austincc.edu>
2024-10-23 21:55:22 +0000

    continuation of vectors

--- src/examples/07_vector/01_create/main.cpp
+++ src/examples/07_vector/01_create/main.cpp
@@ -6,6 +6,14 @@ using std::vector;
 
 int main()
 {
+
+    vector<int> nums{7, 4, 20};
+    vector<int> nums1;
+    nums1.push_back(25);
+    nums1.pushback(10);
+
+    vector<int> nums2(5, 10);
+    
     vector<int> nums{1, 5, 3};
     cout<<"Size of nums: "<<nums.size()<<"\n";
     cout<<"Capacity of nums: "<<nums.capacity<<"\n"();
--- src/examples/07_vector/03_for_ranged/main.cpp
+++ src/examples/07_vector/03_for_ranged/main.cpp
@@ -3,5 +3,6 @@ int main()
 {
     loop_vector_w_while();
     loop_vector_w_for();
+    loop_vector_w_for_range();
     return 0;
 }
