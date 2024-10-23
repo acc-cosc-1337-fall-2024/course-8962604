610fe834915455035d1e2c30d2ae70253f8027a7
8962604 <omar.farooq@g.austincc.edu>
2024-10-23 11:33:04 +0000

    default parameters

--- src/examples/05_functions/05_default_params/default.cpp
+++ src/examples/05_functions/05_default_params/default.cpp
@@ -7,3 +7,11 @@ double get_total1(int qty = 5, double price = 10, double bonus = 100)
 {
     return qty * price + bonus;
 }
+double get_gross_pay(int hours, double rate)
+{
+    return hours * rate;
+}
+double get_gross_pay(int hours, double rate, double bonus)
+{
+    return hours * rate + bonus;
+}
