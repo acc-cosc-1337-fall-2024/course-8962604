196a02869da20f1f493e6330baecef3f90435a1b
8962604 <omar.farooq@g.austincc.edu>
2024-11-04 14:00:03 +0000

    use account in main

--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -1,8 +1,17 @@
 #include "bank_account.h"
 
+using std::cin; using std::cout;
+
 int main()
+
 {
-	Account account;
+	auto balance = 0;
+	cout<<"Enter an amount: ";
+	cin>>balance;
+
+	Account account(balance); // represents a customer account
+
+	cout<<account.get_balance()<<"\n";
 
 	return 0;
 }
