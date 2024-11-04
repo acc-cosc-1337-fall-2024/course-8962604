e714ddf9a940a5b36a17925345b4604eb4bd8d89
8962604 <omar.farooq@g.austincc.edu>
2024-11-04 21:36:47 +0000

    account changes

--- src/examples/08_classes/01_bank/atm.cpp
+++ src/examples/08_classes/01_bank/atm.cpp
@@ -8,8 +8,14 @@ void ATM::display_balance()
 void ATM::make_deposit()
 {
     auto amount = 0;
+    cout<<"Enter deposit ammount";
+    cin>>amount;
+    account.deposit(amount);
 }
 void ATM::make_withdraw()
 {
-
+    auto amount = 0;
+    cout<<"Enter withdraw ammount";
+    cin>>amount;
+    account.withdraw(amount);
 }
