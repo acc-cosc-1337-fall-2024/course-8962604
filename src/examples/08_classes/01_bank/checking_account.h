3d5f47a28b8feb1849ee9568c00e1cedf2d6a051
8962604 <omar.farooq@g.austincc.edu>
2024-11-11 09:59:14 +0000

    create checking account

--- src/examples/08_classes/01_bank/checking_account.h
+++ src/examples/08_classes/01_bank/checking_account.h
@@ -1,1 +1,13 @@
 //checking_account.h
+#include "bank_account.h"
+#ifndef CheckingAccount_H
+#define CheckingAccount_H
+
+
+class CheckingAccount: public Account
+{
+public:
+    CheckingAccount(){}
+    CheckingAccount(int b) :Account(b){}
+};
+#endif
--- test/examples/08_classes/08_classes_tests.cpp
+++ test/examples/08_classes/08_classes_tests.cpp
@@ -1,22 +1,23 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "bank_account.h"
+#include "checking_account.h"
 #include <ctime>
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
-TEST_CASE("Test Account get balance")
+TEST_CASE("Test checking Account get balance")
 {
 	
-	Account account;
+	CheckingAccount account; 
 
 	REQUIRE(account.get_balance() >= 1);
 	REQUIRE(account.get_balance() <= 10000);
 }
-TEST_CASE("Test account 1 param constructor")
+TEST_CASE("Test checking account 1 param constructor")
 {
-	Account account(500); //assume 500 came from the data
+	CheckingAccount account(500); //assume 500 came from the data
 
 	REQUIRE(500 == account.get_balance());
 
