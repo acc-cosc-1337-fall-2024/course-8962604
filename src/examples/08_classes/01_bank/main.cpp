dec65077217364655e615b7569db60988a1a1ace
8962604 <omar.farooq@g.austincc.edu>
2024-11-13 16:46:07 +0000

    unique pointer exp

--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -2,16 +2,20 @@
 #include "checking_account.h"
 #include "savings_account.h"
 #include<vector>
+#include<memory>
 
 using std::cin; using std::cout;
-using std::vector;
+using std::vector; using std::unique_ptr; using std::make_unique;
 
 int main()
 
 	
 {
+
+	unique_ptr<int> num = make_unique<int>(10);
+	cout<<*num<<"\n";
 	
-	Account account = get_account_by_value();
+	/*Account account = get_account_by_value();
 	Account account; //object/instance(variable)
 	display_balance(account);
 	cout<<"Main: "<<account.get_balance()<<"\n";
@@ -54,5 +58,5 @@ int main()
 	{
 	cout<<account.get_balance()<<"\n";
 	}
-	return 0;
+	return 0;*/
 }
